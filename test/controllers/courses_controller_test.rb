6e98fdb50cce42ad2eb2e0fcab84bf7fc9e52a20
IsmaelNicolas <ismael.cedillo@outlook.es>
2024-06-09 12:25:38 -0500

    Add couses test

--- test/controllers/courses_controller_test.rb
+++ test/controllers/courses_controller_test.rb
@@ -1,48 +1,61 @@
-require "test_helper"
+require 'test_helper'
 
 class CoursesControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @course = courses(:one)
-  end
-
-  test "should get index" do
-    get courses_url
+  test 'render a list of courses' do
+    get courses_path
     assert_response :success
+    assert_select '.course', 2
   end
 
-  test "should get new" do
-    get new_course_url
+  test 'render a detailed course' do
+    get courses_path(courses(:course1))
     assert_response :success
+    assert_select '.title', 'Learnig Ruby'
+    assert_select '.description', 'This is a course to learn Ruby'
   end
 
-  test "should create course" do
-    assert_difference("Course.count") do
-      post courses_url, params: { course: { description: @course.description, title: @course.title } }
-    end
+  # setup do
+  #   @course = courses(:one)
+  # end
 
-    assert_redirected_to course_url(Course.last)
-  end
+  # test "should get index" do
+  #   get courses_url
+  #   assert_response :success
+  # end
 
-  test "should show course" do
-    get course_url(@course)
-    assert_response :success
-  end
+  # test "should get new" do
+  #   get new_course_url
+  #   assert_response :success
+  # end
 
-  test "should get edit" do
-    get edit_course_url(@course)
-    assert_response :success
-  end
+  # test "should create course" do
+  #   assert_difference("Course.count") do
+  #     post courses_url, params: { course: { description: @course.description, title: @course.title } }
+  #   end
 
-  test "should update course" do
-    patch course_url(@course), params: { course: { description: @course.description, title: @course.title } }
-    assert_redirected_to course_url(@course)
-  end
+  #   assert_redirected_to course_url(Course.last)
+  # end
 
-  test "should destroy course" do
-    assert_difference("Course.count", -1) do
-      delete course_url(@course)
-    end
+  # test "should show course" do
+  #   get course_url(@course)
+  #   assert_response :success
+  # end
 
-    assert_redirected_to courses_url
-  end
+  # test "should get edit" do
+  #   get edit_course_url(@course)
+  #   assert_response :success
+  # end
+
+  # test "should update course" do
+  #   patch course_url(@course), params: { course: { description: @course.description, title: @course.title } }
+  #   assert_redirected_to course_url(@course)
+  # end
+
+  # test "should destroy course" do
+  #   assert_difference("Course.count", -1) do
+  #     delete course_url(@course)
+  #   end
+
+  #   assert_redirected_to courses_url
+  # end
 end
