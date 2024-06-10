ff9238a8fe057b40c9781ca2dcdfc5970228c3d4
IsmaelNicolas <ismael.cedillo@outlook.es>
2024-06-09 20:37:05 -0500

    Refactor and clean code

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,7 +3,7 @@ class CoursesController < ApplicationController
 
   # GET /courses or /courses.json
   def index
-    @courses = Course.all
+    @courses = Course.all.with_attached_image
   end
 
   # GET /courses/1 or /courses/1.json
